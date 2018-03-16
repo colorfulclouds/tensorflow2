c86b855c58f9a8b03de4b1b6fbf883627f3d7ee2
colorfulclouds <1228794458@qq.com>
2018-03-16 23:35:12 +0800

    add

---
+++ kaggle/titanic/titanic.py
@@ -0,0 +1,158 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC , LinearSVC
+from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier
+
+
+# In[5]:
+
+
+from sklearn.preprocessing import Imputer , Normalizer , scale
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_selection import RFECV
+
+
+# In[6]:
+
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+
+
+# In[7]:
+
+
+mpl.style.use('ggplot')
+sns.set_style('white')
+pylab.rcParams['figure.figsize'] = 8 , 6
+
+
+# In[8]:
+
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+full = train.append(test , ignore_index = True)
+titanic = full[:891]
+
+del train , test
+print('full:' , full.shape , 'titanic:' , titanic.shape)
+
+
+# In[10]:
+
+
+titanic.head()
+
+
+# In[11]:
+
+
+titanic.describe()
+
+
+# In[18]:
+
+
+def plot_histograms( df , variables , n_rows , n_cols ):
+    fig = plt.figure( figsize = ( 16 , 12 ) )
+    for i, var_name in enumerate( variables ):
+        ax=fig.add_subplot( n_rows , n_cols , i+1 )
+        df[ var_name ].hist( bins=10 , ax=ax )
+        ax.set_title( 'Skew: ' + str( round( float( df[ var_name ].skew() ) , ) ) ) # + ' ' + var_name ) #var_name+" Distribution")
+        ax.set_xticklabels( [] , visible=False )
+        ax.set_yticklabels( [] , visible=False )
+    fig.tight_layout()  # Improves appearance a bit.
+    plt.show()
+
+def plot_distribution( df , var , target , **kwargs ):
+    row = kwargs.get( 'row' , None )
+    col = kwargs.get( 'col' , None )
+    facet = sns.FacetGrid( df , hue=target , aspect=4 , row = row , col = col )
+    facet.map( sns.kdeplot , var , shade= True )
+    facet.set( xlim=( 0 , df[ var ].max() ) )
+    facet.add_legend()
+
+def plot_categories( df , cat , target , **kwargs ):
+    row = kwargs.get( 'row' , None )
+    col = kwargs.get( 'col' , None )
+    facet = sns.FacetGrid( df , row = row , col = col )
+    facet.map( sns.barplot , cat , target )
+    facet.add_legend()
+
+def plot_correlation_map( df ):
+    corr = titanic.corr()
+    _ , ax = plt.subplots( figsize =( 12 , 10 ) )
+    cmap = sns.diverging_palette( 220 , 10 , as_cmap = True )
+    _ = sns.heatmap(
+        corr, 
+        cmap = cmap,
+        square=True, 
+        cbar_kws={ 'shrink' : .9 }, 
+        ax=ax, 
+        annot = True, 
+        annot_kws = { 'fontsize' : 12 }
+    )
+
+def describe_more( df ):
+    var = [] ; l = [] ; t = []
+    for x in df:
+        var.append( x )
+        l.append( len( pd.value_counts( df[ x ] ) ) )
+        t.append( df[ x ].dtypes )
+    levels = pd.DataFrame( { 'Variable' : var , 'Levels' : l , 'Datatype' : t } )
+    levels.sort_values( by = 'Levels' , inplace = True )
+    return levels
+
+def plot_variable_importance( X , y ):
+    tree = DecisionTreeClassifier( random_state = 99 )
+    tree.fit( X , y )
+    plot_model_var_imp( tree , X , y )
+    
+def plot_model_var_imp( model , X , y ):
+    imp = pd.DataFrame( 
+        model.feature_importances_  , 
+        columns = [ 'Importance' ] , 
+        index = X.columns 
+    )
+    imp = imp.sort_values( [ 'Importance' ] , ascending = True )
+    imp[ : 10 ].plot( kind = 'barh' )
+    print (model.score( X , y ))
+
+
+# In[19]:
+
+
+plot_correlation_map(titanic)
+
+
+# In[20]:
+
+
+plot_distribution(titanic , var='Age' , target='Survived' , row='Sex')
+
